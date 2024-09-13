380c4eb399a7aea9892d68951b4bccec0d6dc833
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-09-13 11:16:48 +0200

    create new channel

---
+++ src/features/channels/components/create-channel-modal.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useCreateChannelModal } from "../store/use-create-channel-modal";
+import { useCreateChannel } from "../api/use-create-channel";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+export const CreateChannelModal = () => {
+  const router = useRouter();
+
+  const workspaceId = useWorkspaceId();
+
+  const [open, setOpen] = useCreateChannelModal();
+  const [name, setName] = useState("");
+
+  const { mutate, isPending } = useCreateChannel();
+
+  const handleClose = () => {
+    setOpen(false);
+    setName("");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLocaleLowerCase();
+
+    setName(value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    mutate(
+      { name, workspaceId },
+      {
+        onSuccess(id) {
+          router.push(`/channel/${id}`);
+          toast.success("Cahnnel created");
+          handleClose();
+        },
+      }
+    );
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a channel</DialogTitle>
+        </DialogHeader>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            onChange={handleChange}
+            disabled={isPending}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="e.g. plan-meeting"
+          />
+
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
