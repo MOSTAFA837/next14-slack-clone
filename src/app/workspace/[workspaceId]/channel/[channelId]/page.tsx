738c722117b4aa118b3ce0d456a4886653d075f9
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-09-14 12:31:26 +0200

    editor component

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import NotFound from "@/components/not-found";
 import useGetChannel from "@/features/channels/api/use-get-channel";
 import { useChannelId } from "@/hooks/use-channel-id";
 import Header from "./header";
+import ChatInput from "./chat-input";
 
 function ChannelIdPage() {
   const channelId = useChannelId();
@@ -24,6 +25,8 @@ function ChannelIdPage() {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
+      <div className="flex-1" />
+      <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   );
 }
