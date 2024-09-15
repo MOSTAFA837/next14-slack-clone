cc695af0b793c95140e8bb633eb137742152ca30
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-09-15 13:10:07 +0200

    image upload

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,3 +1,4 @@
+import { useGenerateUploadUrl } from "@/features/apload/api/use-generate-upload-url";
 import { useCreateMessage } from "@/features/messages/api/use-create-message";
 import { useChannelId } from "@/hooks/use-channel-id";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
@@ -5,12 +6,20 @@ import dynamic from "next/dynamic";
 import Quill from "quill";
 import { useRef, useState } from "react";
 import { toast } from "sonner";
+import { Id } from "../../../../../../convex/_generated/dataModel";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
 interface ChatInputProps {
   placeholder: string;
 }
+
+type CreateMessageValues = {
+  channelId: Id<"channels">;
+  workspaceId: Id<"workspaces">;
+  body: string;
+  image: Id<"_storage"> | undefined;
+};
 
 function ChatInput({ placeholder }: ChatInputProps) {
   const [editorKey, setEditorKey] = useState(0);
@@ -22,6 +31,7 @@ function ChatInput({ placeholder }: ChatInputProps) {
   const channelId = useChannelId();
 
   const { mutate: createMessage } = useCreateMessage();
+  const { mutate: generateUploadUrl } = useGenerateUploadUrl();
 
   const handleSubmit = async ({
     body,
@@ -32,16 +42,43 @@ function ChatInput({ placeholder }: ChatInputProps) {
   }) => {
     try {
       setIsPending(true);
-      await createMessage(
-        { workspaceId, channelId, body },
-        { throwError: true }
-      );
+      editorRef?.current?.enable(false);
+
+      const values: CreateMessageValues = {
+        channelId,
+        workspaceId,
+        body,
+        image: undefined,
+      };
+
+      if (image) {
+        const url = await generateUploadUrl({}, { throwError: true });
+
+        if (!url) throw new Error("Failed to generate upload url");
+
+        const result = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": image.type,
+          },
+          body: image,
+        });
+
+        if (!result.ok) throw new Error("Failed to upload image");
+
+        const { storageId } = await result.json();
+
+        values.image = storageId;
+      }
+
+      await createMessage(values, { throwError: true });
 
       setEditorKey((prev) => prev + 1);
     } catch (error) {
       toast.error("Failed to send message");
     } finally {
       setIsPending(false);
+      editorRef?.current?.enable(true);
     }
   };
 
