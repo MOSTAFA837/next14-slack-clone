eb61b1afaf40eadc3c9758d6ae7b238a91a890c5
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-11-17 23:16:07 +0200

    search dialog

--- src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
+++ src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -6,12 +6,15 @@ import { Loader } from "lucide-react";
 import Header from "./header";
 import ChatInput from "./chat-input";
 import MessageList from "@/components/message-list";
+import { usePanel } from "@/hooks/use-panel";
 
 interface ConversationProps {
   id: Id<"conversations">;
 }
 function Conversation({ id }: ConversationProps) {
   const memberId = useMemberId();
+
+  const { onOpenProfile } = usePanel();
 
   const { data: member, isLoading: memberLoading } = useGetMember({
     id: memberId,
@@ -32,7 +35,7 @@ function Conversation({ id }: ConversationProps) {
       <Header
         memberName={member?.user.name}
         memberImage={member?.user.image}
-        onClick={() => {}}
+        onClick={() => onOpenProfile(memberId)}
       />
 
       <MessageList
